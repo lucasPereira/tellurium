85d146e5fe88a6684dee026b25164c1655c10375
Lucas Pereira da Silva <lucas.pereira@ufsc.br>
2018-09-12 09:23:14 -0300

    Adding actions and refactoring announcements.

--- static/js/tellurium-announcement.js
+++ static/js/tellurium-announcement.js
@@ -4,26 +4,70 @@ class TelluriumAnnouncementElement extends TelluriumElement {
 
 	constructor() {
 		super();
-		this.open = true;
-		let shadow = this.attachShadow({ mode: 'open' });
-		let elementDocument = document.currentScript.ownerDocument;
-		let template = elementDocument.querySelector('template');
+		this.announcements = 0;
+		this.templateDocument = document.currentScript.ownerDocument;
+		this.initializeTemplate();
+		this.startListeners();
+	}
+
+	initializeTemplate() {
+		this.attachShadow({ mode: 'open' });
+		let template = this.templateDocument.querySelector('template');
 		let content = template.content.cloneNode(true);
-		shadow.appendChild(content);
+		this.shadowRoot.appendChild(content);
+	}
+
+	startListeners() {
 		Tellurium.messenger.subscribe('route-not-found', () => {
-			let templateRouteNotFound = elementDocument.querySelector('template#route-not-found');
-			let contentRouteNotFound = templateRouteNotFound.content.cloneNode(true);
-			shadow.querySelector('p').textContent = contentRouteNotFound.textContent;
-			shadow.querySelector('ul').appendChild(contentRouteNotFound);
+			this.showError('template#route-not-found')
 		});
-		shadow.querySelector('button').addEventListener('click', () => {
-			if (this.open) {
-				shadow.querySelector('ul').style.display = 'none';
-			} else {
-				shadow.querySelector('ul').style.display = 'block';
+		this.shadowRoot.querySelector('button#collapse').addEventListener('click', () => {
+			this.shadowRoot.querySelector('div#container').classList.add('collapsed');
+			this.shadowRoot.querySelector('div#container').classList.remove('expanded');
+		});
+		this.shadowRoot.querySelector('button#expand').addEventListener('click', () => {
+			this.shadowRoot.querySelector('div#container').classList.add('expanded');
+			this.shadowRoot.querySelector('div#container').classList.remove('collapsed');
+		});
+		this.shadowRoot.querySelector('button#clear').addEventListener('click', () => {
+			if (this.announcements > 0) {
+				let containerElement = this.shadowRoot.querySelector('div#container');
+				let announcementsElement = this.shadowRoot.querySelector('div#container ul');
+				let lastAnnouncementElement = this.shadowRoot.querySelector('div#container p');
+				containerElement.removeChild(announcementsElement);
+				containerElement.removeChild(lastAnnouncementElement);
+				this.announcements = 0;
 			}
-			this.open = !this.open;
 		});
+		this.shadowRoot.querySelector('button#close').addEventListener('click', () => {
+			this.shadowRoot.querySelector('div#container').classList.add('closed');
+		});
+	}
+
+	showError(templateSelector) {
+		if (this.announcements === 0) {
+			let announcementsTemplate = this.templateDocument.querySelector('template#announcements').content.cloneNode(true);
+			let lastAnnouncementTemplate = this.templateDocument.querySelector('template#last-announcement').content.cloneNode(true);
+			this.shadowRoot.querySelector('div#container').appendChild(lastAnnouncementTemplate);
+			this.shadowRoot.querySelector('div#container').appendChild(announcementsTemplate);
+		}
+		let containerElement = this.shadowRoot.querySelector('div#container');
+		let announcementsElement = this.shadowRoot.querySelector('div#container ul');
+		let lastAnnouncementElement = this.shadowRoot.querySelector('div#container p');
+		let announcementTemplate = this.templateDocument.querySelector(templateSelector).content.cloneNode(true);
+		if (this.announcements > 0) {
+			announcementsElement.insertBefore(announcementTemplate, announcementsElement.firstChild);
+		} else {
+			announcementsElement.appendChild(announcementTemplate);
+		}
+		let announcementElement = this.shadowRoot.querySelector('div#container ul li:first-child');
+		lastAnnouncementElement.textContent = announcementElement.textContent;
+		containerElement.classList.remove('success');
+		lastAnnouncementElement.classList.remove('success');
+		containerElement.classList.add('error');
+		lastAnnouncementElement.classList.add('error');
+		containerElement.classList.remove('closed');
+		this.announcements++;
 	}
 
 }
